b375a1325efc3a1aeb4301a0b80b6c17a5f2315a
Daniel Mason <daniel.mason@apolitical.co>
2018-07-24 11:44:26 +0100

    Added: Attempting to sign, but problems with helm

--- roll-charts.sh
+++ roll-charts.sh
@@ -3,9 +3,29 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-GIT_REPO=`git remote -v | awk '{ print $2 }' | head -n 1`
+get_gpg_key() {
+  KEY_NAME=${1:-}
+  if [ ! -z "${KEY_NAME}" ]; then
+    KEY=`gpg --list-keys ${KEY_NAME} | head -n 2 | tail -n 1 | awk '{ print $1 }'`
+  else
+    KEY=`gpgconf --list-options gpg | awk -F: '$1 == "default-key" {print $10}'`
+  fi
+  echo ${KEY}
+}
+
 BUILD_DIR="${PWD}/build"
 CHARTS_DIR="${PWD}/charts"
+GIT_REPO=`git remote -v | awk '{ print $2 }' | head -n 1`
+KEY_NAME=${1:-}
+GPG_KEY=$(get_gpg_key "${KEY_NAME}")
+if [ -z "${GPG_KEY}" ]; then
+    echo "No GPG key found!"
+    echo "Either set a default key in GPG or specify one when running this command:"
+    echo
+    echo "Eg:"
+    echo "./roll-charts.sh daniel.mason@apolitical.co"
+    exit 1
+fi
 
 echo
 echo "Configuring build directory"
@@ -53,7 +73,7 @@ echo
 (
     cd ${CHARTS_DIR}
     for package_name in *; do
-      helm package "${package_name}" -d "${BUILD_DIR}"
+      helm package "${package_name}" -d "${BUILD_DIR}" --sign --key "${GPG_KEY}"
     done
 
     echo "Indexing"
@@ -79,16 +99,16 @@ echo "Committing changes"
 echo "=================="
 echo
 
-(
-    cd ${BUILD_DIR}
-
-    if [ `git status -s | grep -v index.yaml` ]; then
-        echo "Committing"
-        git add .
-        DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
-        git commit -m "Build ${DATE}"
-        echo "Done"
-    else
-        echo "No changes"
-    fi
-)
+#(
+#    cd ${BUILD_DIR}
+#
+#    if [ `git status -s | grep -v index.yaml` ]; then
+#        echo "Committing"
+#        git add .
+#        DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
+#        git commit -m "Build ${DATE}"
+#        echo "Done"
+#    else
+#        echo "No changes"
+#    fi
+#)
